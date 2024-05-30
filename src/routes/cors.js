bc993eec013385ae01713f04fe0a9996c5e6d318
Rynare <rynare.of.legend@gmail.com>
2024-05-31 05:17:05 +0700

    <update>[cors] menambahkan allowed cors url

--- src/routes/cors.js
+++ src/routes/cors.js
@@ -2,7 +2,7 @@ const CORS_OPTION = {
     origin: [
         'http://localhost:3000',
         'https://api-resepnya.vercel.app',
-        "http://127.0.0.1:5500"
+        'http://localhost:8080'
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
