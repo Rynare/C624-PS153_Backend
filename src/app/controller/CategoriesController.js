1dc940c0776a44054929d994cfd0d72d19a1aa9a
Rynare <rynare.of.legend@gmail.com>
2024-05-30 14:56:57 +0700

    update categories-controller

--- src/app/controller/CategoriesController.js
+++ src/app/controller/CategoriesController.js
@@ -1,17 +1,52 @@
 const { getResepnya } = require("../../services/fetchResepnya")
+const { KulineryDB } = require("../database/KulineryDB")
 
 const CategoriesController = {
     getRecipesCategory: async (req, res) => {
-        const response = await getResepnya(req, res, "/api/recipes/categories")
-        res.status(200).json({
-            ...response.data
+        const response = await KulineryDB.findDatas({
+            table_name: "recipe_categories",
+            options: {
+                projection: {
+                    _id: 0,
+                }
+            }
         })
+        if (response.length >= 1) {
+            return res.status(200).json({
+                method: req.method,
+                status: true,
+                results: response
+            })
+        } else {
+            return res.status(500).json({
+                method: req.method,
+                status: false,
+                results: response
+            })
+        }
     },
     getArticleCategory: async (req, res) => {
-        const response = await getResepnya(req, res, "/api/articles/categories")
-        res.status(200).json({
-            ...response.data
+        const response = await KulineryDB.findDatas({
+            table_name: "article_categories",
+            options: {
+                projection: {
+                    _id: 0,
+                }
+            }
         })
+        if (response.length >= 1) {
+            return res.status(200).json({
+                method: req.method,
+                status: true,
+                results: response
+            })
+        } else {
+            return res.status(500).json({
+                method: req.method,
+                status: false,
+                results: response
+            })
+        }
     }
 }
 
